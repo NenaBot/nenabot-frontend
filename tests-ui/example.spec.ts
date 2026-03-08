21f7f6b6eca1870e43219a94290943b7f7a4ecd6
Aleksi Kirjavainen <aleksi.kirjavainen@tuni.fi>
2026-03-08 18:16:19 +0200

    fix prettier errors

--- tests-ui/example.spec.ts
+++ tests-ui/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has title', async ({ page }) => {
-    await page.goto('/');
-    await page.waitForTimeout(3000);
-    await expect(page).toHaveTitle("NenäBot - Battery Inspection System");
-    await page.waitForTimeout(3000);
+  await page.goto('/');
+  await page.waitForTimeout(3000);
+  await expect(page).toHaveTitle('NenäBot - Battery Inspection System');
+  await page.waitForTimeout(3000);
 });
